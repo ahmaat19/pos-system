c63eb4e678582028ae79c9e4ac85ce15338f019e
Ahmed <ahmaat19@gmail.com>
2021-11-04 17:06:59 +0300

    udpating homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import HomePage from '../components/home/Home'
+import Head from 'next/head'
 
 const Home = () => {
-  return <HomePage />
+  return (
+    <>
+      <Head>
+        <title>Ligo Medical</title>
+        <meta property='og:title' content='Ligo Medical' key='title' />
+      </Head>
+
+      <HomePage />
+    </>
+  )
 }
 
 export default Home
